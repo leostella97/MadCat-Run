4e2ac791795481ce37502180d93377a17a39e116
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2024-02-29 15:23:54 -0300

    allow-empty

--- main.py
+++ main.py
@@ -19,11 +19,16 @@ rat_img = pygame.transform.scale(pygame.image.load(os.path.join(pasta_imagens, "
 posicao_gato = [100, 300]
 posicao_rato = [600, 300]
 
-# Defina a velocidade de movimento
-velocidade = 1
+# Defina a velocidade de movimento do jogador e do inimigo
+velocidade_jogador = 5
+velocidade_inimigo = 3
 
 # Variável para controlar qual personagem está sendo controlado
 personagem_controlado = None
+
+# Variáveis para controlar o tempo
+tempo_inicial = pygame.time.get_ticks()  # Tempo em milissegundos
+tempo_limite = 60000  # 1 minuto em milissegundos
 
 # Função para desenhar os personagens na tela inicial
 def desenhar_tela_inicial():
@@ -47,7 +52,7 @@ def desenhar_personagens():
     janela.blit(rat_img, posicao_rato)
 
 # Função para controlar o movimento do personagem
-def controlar_personagem(teclas, posicao):
+def controlar_personagem(teclas, posicao, velocidade):
     if teclas[pygame.K_w] or teclas[pygame.K_UP]:
         posicao[1] -= velocidade
     if teclas[pygame.K_a] or teclas[pygame.K_LEFT]:
@@ -66,6 +71,24 @@ def controlar_personagem(teclas, posicao):
         posicao[1] = 0
     elif posicao[1] > altura_janela - 50:  # 50 é a altura do personagem
         posicao[1] = altura_janela - 50
+
+# Função para verificar colisões entre o jogador e o inimigo
+def verificar_colisoes():
+    global personagem_controlado
+
+    if personagem_controlado == "rato":
+        if pygame.Rect(posicao_rato, (50, 50)).colliderect(pygame.Rect(posicao_gato, (50, 50))):
+            return True  # Colisão entre rato e gato
+    elif personagem_controlado == "gato":
+        if pygame.Rect(posicao_gato, (50, 50)).colliderect(pygame.Rect(posicao_rato, (50, 50))):
+            return True  # Colisão entre gato e rato
+    return False
+
+# Função para exibir mensagens na tela
+def exibir_mensagem(texto):
+    fonte = pygame.font.SysFont(None, 40)
+    texto_renderizado = fonte.render(texto, True, (255, 0, 0))
+    janela.blit(texto_renderizado, (250, 250))
 
 # Loop principal do jogo
 tela_inicial = True
@@ -94,10 +117,39 @@ while rodando:
     elif jogo_iniciado:
         teclas = pygame.key.get_pressed()
         if personagem_controlado == "rato":
-            controlar_personagem(teclas, posicao_rato)
+            controlar_personagem(teclas, posicao_rato, velocidade_jogador)
+            # Fazer o gato perseguir o rato
+            if posicao_gato[0] < posicao_rato[0]:
+                posicao_gato[0] += velocidade_inimigo
+            elif posicao_gato[0] > posicao_rato[0]:
+                posicao_gato[0] -= velocidade_inimigo
+            if posicao_gato[1] < posicao_rato[1]:
+                posicao_gato[1] += velocidade_inimigo
+            elif posicao_gato[1] > posicao_rato[1]:
+                posicao_gato[1] -= velocidade_inimigo
         elif personagem_controlado == "gato":
-            controlar_personagem(teclas, posicao_gato)
+            controlar_personagem(teclas, posicao_gato, velocidade_jogador)
+            # Fazer o rato fugir do gato
+            if posicao_rato[0] < posicao_gato[0]:
+                posicao_rato[0] += velocidade_inimigo
+            elif posicao_rato[0] > posicao_gato[0]:
+                posicao_rato[0] -= velocidade_inimigo
+            if posicao_rato[1] < posicao_gato[1]:
+                posicao_rato[1] += velocidade_inimigo
+            elif posicao_rato[1] > posicao_gato[1]:
+                posicao_rato[1] -= velocidade_inimigo
 
+        # Verificar colisões
+        if verificar_colisoes():
+            exibir_mensagem("Você perdeu!")
+        else:
+            # Verificar tempo
+            tempo_atual = pygame.time.get_ticks()
+            if tempo_atual - tempo_inicial >= tempo_limite:
+                if personagem_controlado == "rato":
+                    exibir_mensagem("Você venceu!")
+                else:
+                    exibir_mensagem("Você perdeu!")
         # Limpe a tela
         janela.fill((255, 255, 255))
 
