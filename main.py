063e83092a23c0ef3f089c003bb56d03a97936de
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2024-02-29 15:03:09 -0300

    allow-empty

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ altura_janela = 600
 janela = pygame.display.set_mode((largura_janela, altura_janela))
 pygame.display.set_caption("MadCat Run")
 
-# Carregue as imagens
+# Carregue as imagens e dimensione-as
 pasta_imagens = "img"
-cat_img = pygame.image.load(os.path.join(pasta_imagens, "cat.png"))
-rat_img = pygame.image.load(os.path.join(pasta_imagens, "rat.png"))
+cat_img = pygame.transform.scale(pygame.image.load(os.path.join(pasta_imagens, "cat.png")), (50, 50))
+rat_img = pygame.transform.scale(pygame.image.load(os.path.join(pasta_imagens, "rat.png")), (50, 50))
 
 # Defina a posição inicial do gato e do rato
 posicao_gato = [100, 300]
 posicao_rato = [600, 300]
 
 # Defina a velocidade de movimento
-velocidade = 5
+velocidade = 1
 
 # Variável para controlar qual personagem está sendo controlado
 personagem_controlado = None
@@ -56,6 +56,16 @@ def controlar_personagem(teclas, posicao):
         posicao[1] += velocidade
     if teclas[pygame.K_d] or teclas[pygame.K_RIGHT]:
         posicao[0] += velocidade
+
+    # Verifique se o personagem atingiu os limites da tela
+    if posicao[0] < 0:
+        posicao[0] = 0
+    elif posicao[0] > largura_janela - 50:  # 50 é a largura do personagem
+        posicao[0] = largura_janela - 50
+    if posicao[1] < 0:
+        posicao[1] = 0
+    elif posicao[1] > altura_janela - 50:  # 50 é a altura do personagem
+        posicao[1] = altura_janela - 50
 
 # Loop principal do jogo
 tela_inicial = True
